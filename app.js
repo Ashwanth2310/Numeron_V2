a2bc293a434b569e8baf28324fb5734ed53eead2
Ashwanth2310 <ashwanth@rajakumar.net>
2023-11-18 14:53:27 +0530

    updates

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-// Iteration 1: Making the play button in the index.html functional.
-// Description: When the play button is clicked the game.html page should be opened
+// Iteration 1: Make the Play button functional.
+// Description: When the Play button is clicked the game page should be displayed.
+const playButton = document.getElementById("play-button");
+
+playButton.onclick = () =>{
+    location.href = "game.html"
+};
+
